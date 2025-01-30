1abc3ca3c9e002b78d8051bb9b8a9c8cfcea3205
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2025-01-30 18:22:07 +0800

    Create 1791. Find Center of Star Graph.cpp

---
+++ 1791. Find Center of Star Graph.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        vector<int> edge1 = edges[0];
+        vector<int> edge2 = edges[1];
+
+        if (edge1[0] == edge2[0] || edge1[0] == edge2[1])
+            return edge1[0];
+        return edge1[1];
+    }
+};
