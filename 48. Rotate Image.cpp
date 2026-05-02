09dd88c41398e85ad52bcfc9046c6eb83a96c929
asteroid5566 <47234920+asteroid5566@users.noreply.github.com>
2026-05-03 03:21:48 +0800

    Create 48. Rotate Image.cpp

---
+++ 48. Rotate Image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int i, j, n = matrix.size();
+
+        for (i = 0; i < n; i++) {
+            for (j = i + 1; j < n; j++)
+                swap(matrix[i][j], matrix[j][i]);
+        }
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n / 2; j++)
+                swap(matrix[i][j], matrix[i][n - j - 1]);
+        }
+    }
+};
